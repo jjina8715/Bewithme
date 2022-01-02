558da370d08c18b28bb9e211313ce7423c67b464
jjina8715 <junjina63@naver.com>
2022-01-02 16:23:14 +0900

    init project

---
+++ src/main/java/com/bewithme/BewithmeApplication.java
@@ -0,0 +1,13 @@
+package com.bewithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BewithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BewithmeApplication.class, args);
+	}
+
+}
