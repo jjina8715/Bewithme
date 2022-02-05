8cb4e884e7a5795cde0ca306b69abc58ad8601bc
minwoo <alsdn114560@gmail.com>
2022-02-05 18:07:45 +0900

    Revert "css folder"
    This reverts commit 69ab089cc1aa90afdc2cc26380596afa1d1e846e.

--- src/main/java/com/bewithme/app/configuration/security/SercurityConfig.java
+++ src/main/java/com/bewithme/app/configuration/security/SercurityConfig.java
@@ -33,7 +33,7 @@ public class SercurityConfig extends WebSecurityConfigurerAdapter{
 		.userDetailsService(userDetailsService)
 		.authorizeRequests()
 			.antMatchers(URI_RESOURCE_LOCATIONS).permitAll()
-			.antMatchers("/", "/signin", "/signup", "/login/**").permitAll()
+			.antMatchers("/", "/login", "/join", "/login/**").permitAll()
 		.and()
 			.formLogin()
 			.loginPage("/login")
