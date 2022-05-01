aa381fb64b09f4668d6a48be0dec8d389ff5d1de
kkssomm <qwers1415@gmail.com>
2022-05-01 16:21:40 +0900

    Enum 추가

---
+++ src/main/java/com/bewithme/data/type/GameType.java
@@ -0,0 +1,17 @@
+package com.bewithme.data.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum GameType {
+	
+	TYPE_01("TYPE_01", "즐겜"), 
+	TYPE_02("TYPE_02", "빡겜"),  
+	TYPE_03("TYPE_03", "여포"),
+	TYPE_04("TYPE_04", "존버");
+	
+	private String code;
+	private String desc;
+}
