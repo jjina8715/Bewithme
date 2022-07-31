30335104aa332fdecc6f349d0bcef83788bed2b3
jjina8715 <junjina63@naver.com>
2022-07-31 17:05:13 +0900

    함수 삭제

--- src/main/java/com/bewithme/data/repository/MemberBasicRepositoryCutom.java
+++ src/main/java/com/bewithme/data/repository/MemberBasicRepositoryCutom.java
@@ -1,11 +1,5 @@
 package com.bewithme.data.repository;
 
-import java.util.List;
-
-import com.bewithme.app.mate.model.MateInfoDto;
-import com.bewithme.app.member.model.MemberCondition;
-
 public interface MemberBasicRepositoryCutom {
 
-	List<MateInfoDto> readItems(MemberCondition memberCondition);
 }
--- src/main/java/com/bewithme/data/repository/MemberBasicRepositoryImpl.java
+++ src/main/java/com/bewithme/data/repository/MemberBasicRepositoryImpl.java
@@ -1,14 +1,5 @@
 package com.bewithme.data.repository;
 
-import static com.bewithme.data.entity.QMemberBasicEntity.memberBasicEntity;
-import static com.bewithme.data.entity.QMatchingInfoEntity.matchingInfoEntity;
-
-import java.util.List;
-
-import com.bewithme.app.mate.model.MateInfoDto;
-import com.bewithme.app.member.model.MemberCondition;
-import com.bewithme.data.type.StatCode;
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -18,22 +9,5 @@ public class MemberBasicRepositoryImpl implements MemberBasicRepositoryCutom{
 
 	private final JPAQueryFactory jpaQueryFactory;
 	
-	@Override
-	public List<MateInfoDto> readItems(MemberCondition memberCondition) {
-		return jpaQueryFactory.select(Projections.bean(MateInfoDto.class, 
-				memberBasicEntity.id,
-				memberBasicEntity.nickname,
-				memberBasicEntity.gender,
-				memberBasicEntity.accessTime,
-				memberBasicEntity.aboutMe,
-				memberBasicEntity.discordUrl
-			))
-			.from(memberBasicEntity, matchingInfoEntity)
-			.where(memberBasicEntity.id.eq(matchingInfoEntity.requestee.id)
-					.and(matchingInfoEntity.requester.id.eq(memberBasicEntity.id))
-					.and(matchingInfoEntity.stat.eq(StatCode.C01.getCode()))
-			)
-			.orderBy(matchingInfoEntity.createdDate.asc())
-			.fetch();
-	}
+
 }
